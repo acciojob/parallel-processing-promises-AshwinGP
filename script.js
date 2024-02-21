fe70760dc9ebce2f231452ec65735ac64bc97bd8
AshwinGP <111189173+AshwinGP@users.noreply.github.com>
2024-02-21 19:02:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+let promise1=new Promise((res,rej)=>{
+	     setTimeout(()=>{
+			 resolve(`${images.url[0]}`);
+		 },1000)
+});
+
+let promise2=new Promise((res,rej)=>{
+	     setTimeout(()=>{
+			 resolve(`${images.url[1]}`);
+		 },1000)
+});
+
+let promise3=new Promise((res,rej)=>{
+	     setTimeout(()=>{
+			 resolve(`${images.url[1]}`);
+		 },1000)
+});
+ function fetchImg(){
+	 Promise.all([promise1,promise2,promise3]).then((res)=>{
+	 return document.getElementById("output").innerHTML=`<img src="${res}" alt="img">`
+		 }).catch((e)=>{
+		 console.log("error",e);
+		 })
+				   
+ }
